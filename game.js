bbb94247c3f2accc6d40556c359b546808c212b4
kellims <kellisimon18@gmail.com>
2023-03-01 16:58:22 -0700

    Finish Step 1 CSS

---
+++ game.js
@@ -0,0 +1,15 @@
+
+//Define HTML Elements
+
+//Create Players
+
+//Define Winning Conditions
+
+//Define Tie Conditions
+
+//Create Even Listeners
+
+//Create Funtion which updates Scoreboard
+
+//Extended Features: Create input for user to enter their own name, which would automatically update Player 1
+
