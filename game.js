14d8fe854cf32bb2bc548bf08f593ccdbfed39f3
kellims <kellisimon18@gmail.com>
2023-03-02 16:15:58 -0700

    Saving March 1 Progress

--- game.js
+++ game.js
@@ -1,9 +1,33 @@
 
 //Define HTML Elements
+// let cards = document.querySelectorAll('.card');
+// let playerOne = document.querySelector('.player');
+
+//Create Deck of Cards
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+function deckOfCards() {
+    let deck = new Array();
+    for(let i = 0; i < suits.length; i++) {
+        for(let j = 0; j < values.length; j++) {
+            let card = {Value: values[j], Suit: suits[i]};
+            deck.push(card);
+        }
+    }
+    return deck;
+}
+console.log(deckOfCards);
+// let deck
 
 //Create Players
+let computerPlayer = 
 
 //Define Winning Conditions
+
+function winner() {
+
+}
 
 //Define Tie Conditions
 
