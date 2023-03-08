e7b64658a60fe094c5cbdedf0ec2483aea59d651
kellims <kellisimon18@gmail.com>
2023-03-08 12:53:55 -0700

    Updating Game

--- game.js
+++ game.js
@@ -1,23 +1,22 @@
 
 //Define HTML Elements
 let playersCard = document.querySelector('.playersCards');
-// console.log(playersCard);
 // let playerOne = document.querySelector('.player1');
-// console.log(playerOne);
 let computersCard = document.querySelector('.computersCards');
-// console.log(computersCard);
 // let computer = document.querySelector('.computer');
-// console.log(computer);
 let playerScore = document.querySelector('.playerScore');
-// console.log(playerScore);
 let computerScore = document.querySelector('.computerScore');
-// console.log(computerScore);
+
+let newGame = document.querySelector('.newDeck');
+let drawCard = document.querySelector('.draw');
+
+
+
 
 //Make each player an Object - since I'm not adding players to the game, I don't need a class
 let playerOne = {
     name: "Player One",
 }
-
 let playerTwo = {
     name: "Computer",
 }
@@ -26,20 +25,35 @@ console.log(playerTwo);
 
 
 
-//Create classes to store my cards (tip from Office Hours)
+// //Create classes to store my cards (tip from Office Hours)
 const suits = ["♠", "♦", "♣", "♥"];
 const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
+
 class deckOfCards {
-    constructor(cards = newDeck()) {
-        this.cards = cards
+    constructor() {
+        this.cards = [];
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < values.length; j++) { 
+                // this.cards.push(new card(suits[i], values.indexOf(values[j]), values[j]));
+                this.cards.push(new card(suits[i], values[j]));    
+            }
+        } 
+    }
+    shuffleDeck() {
+        for (let i = deck.cards.length - 1; i > 0; i --) {
+            let j = Math.floor(Math.random() * i);
+            let temp = deck.cards[i];
+            deck.cards[i] = deck.cards[j];
+            deck.cards[j] = temp;
+        }
     }
 }
 
 class card {
     constructor(suit, value){
-        this.suit = suit
-        this.value = value
+        this.suit = suit;
+        this.value = value;
     }
     // // add HTML Images for each card.
     htmlForCard(){
@@ -51,63 +65,68 @@ class card {
     }
 }
 
-
-//Creating a new deck using the .flatMap & .map to iterate through array: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap
-function newDeck() {
-    return suits.flatMap(suit => {
-        return values.map(value => {
-            return new card(suit, value)
-        })
-    })
-}
 const deck = new deckOfCards()
-// console.log(deck.cards)
+console.log(deck.cards)
 
 
-//making shuffle function work with cards as a class
-function shuffleDeck() {
-    for (let i = deck.cards.length - 1; i > 0; i --) {
-        let j = Math.floor(Math.random() * i);
-        let temp = deck.cards[i];
-        deck.cards[i] = deck.cards[j];
-        deck.cards[j] = temp;
-    }
-}
-
-// shuffleDeck();
-// console.log(deck);
-
-
-
-//problem I was running into - player 1 & 2 were inside the function, so they weren't global
+//These variable hold each players deck
 let playerOneCards
 let playerTwoCards
 
+
 //Game Function
-function playGame () {
+// newGame.addEventListener("click", playGameOfWar()) {
+playGameOfWar()
+function playGameOfWar () {
     let deck = new deckOfCards;
-    shuffleDeck()
+    deck.shuffleDeck()
+    // Have to figure out why it's not calling my Shuffle function and instead splitting cards in half, in order
     let half = Math.ceil(deck.cards.length / 2);
-    let playerOneCards = deck.cards.splice(0, half);
-    let playerTwoCards = deck.cards.splice(-half);
+    let playerOneCards = deck.cards.slice(0, half);
+    let playerTwoCards = deck.cards.slice(half);
+    console.log(playerOneCards)
+    console.log(playerTwoCards)
 
-    // console.log(`Player One Cards are: ${playerOneCards}`);
-    // console.log(`Computers Cards are: ${playerTwoCards}`);
-
+    cardsRemaining()
+    //function to track the remaining cards in each players deck
     function cardsRemaining () {
-        playerScore.innerText = playerOneCards
-        computerScore.innerText = playerTwoCards
+        playerScore.innerText = playerOneCards.length
+        computerScore.innerText = playerTwoCards.length  
     }
+    
+    drawCard.addEventListener("click", drawCards) 
+    //function for 'Draw' button to reveal new cards for each player
+    function drawCards() {
+        if(playerOneCards.length !== 0 && playerTwoCards !== 0){
+                let playersCard = playerOneCards.shift();
+                let computersCard = playerTwoCards.shift();
+                if(playersCard.values.indexOf(values[j]) > computersCard.values.indexOf(values[j])) {
+                    //Assign both cards to player's deck
+                    playerOneCards.push(card)
+                } else if(computersCard.values.indexOf(values[j]) > playersCard.values.indexOf(values[j])) {
+                    //Assign both cards to computer's deck
+                    playerTwoCards.push(card)
+                }
+                // console.log(playersCard)
+                // return playersCard computersCard
+
+
+//             // for (let i = 0; i < playerOneCards.length; i++)
+            
+//         }
+//     }
+
+       
+
+
+//     // }
+    
 }
-playGame()
+ 
+}
+}
+
+
 
 computersCard.appendChild(deck.cards[0].htmlForCard())
 playersCard.appendChild(deck.cards[0].htmlForCard())
-
-//Function to update Cards Remaining Totals
-
-
-
-
-
-
