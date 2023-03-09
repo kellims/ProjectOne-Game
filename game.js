8aa3afd4f8b1c114edbeb3194558eb73e0c031ad
kellims <kellisimon18@gmail.com>
2023-03-08 19:49:46 -0700

    MVP Project

--- game.js
+++ game.js
@@ -1,16 +1,14 @@
 
 //Define HTML Elements
 let playersCard = document.querySelector('.playersCards');
-// let playerOne = document.querySelector('.player1');
 let computersCard = document.querySelector('.computersCards');
-// let computer = document.querySelector('.computer');
 let playerScore = document.querySelector('.playerScore');
 let computerScore = document.querySelector('.computerScore');
+let gameAlerts = document.querySelector('.alerts');
 
 let newGame = document.querySelector('.newDeck');
 let drawCard = document.querySelector('.draw');
-
-
+// let playerOne = document.querySelector('.player1');
 
 
 //Make each player an Object - since I'm not adding players to the game, I don't need a class
@@ -20,14 +18,13 @@ let playerOne = {
 let playerTwo = {
     name: "Computer",
 }
-console.log(playerOne);
-console.log(playerTwo);
 
 
 
-// //Create classes to store my cards (tip from Office Hours)
+// Create classes to store my cards (tip from Office Hours)
 const suits = ["♠", "♦", "♣", "♥"];
-const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
 
 
 class deckOfCards {
@@ -35,25 +32,17 @@ class deckOfCards {
         this.cards = [];
         for (let i = 0; i < suits.length; i++) {
             for (let j = 0; j < values.length; j++) { 
-                // this.cards.push(new card(suits[i], values.indexOf(values[j]), values[j]));
-                this.cards.push(new card(suits[i], values[j]));    
+                this.cards.push(new card(suits[i], values[j], values.indexOf(values[j+1]))); 
             }
         } 
-    }
-    shuffleDeck() {
-        for (let i = deck.cards.length - 1; i > 0; i --) {
-            let j = Math.floor(Math.random() * i);
-            let temp = deck.cards[i];
-            deck.cards[i] = deck.cards[j];
-            deck.cards[j] = temp;
-        }
     }
 }
 
 class card {
-    constructor(suit, value){
+    constructor(suit, value, number){
         this.suit = suit;
         this.value = value;
+        this.number = number;
     }
     // // add HTML Images for each card.
     htmlForCard(){
@@ -65,22 +54,24 @@ class card {
     }
 }
 
-const deck = new deckOfCards()
-console.log(deck.cards)
+function shuffleDeck(deck) {
+    for (let i = deck.cards.length - 1; i > 0; i --) {
+        let j = Math.floor(Math.random() * i);
+        let temp = deck.cards[i];
+        deck.cards[i] = deck.cards[j];
+        deck.cards[j] = temp;
+        
+    } return deck
+}
 
 
-//These variable hold each players deck
-let playerOneCards
-let playerTwoCards
 
+//Event Listener to begin Game Function
+newGame.addEventListener("click", playGameOfWar);
 
-//Game Function
-// newGame.addEventListener("click", playGameOfWar()) {
-playGameOfWar()
 function playGameOfWar () {
-    let deck = new deckOfCards;
-    deck.shuffleDeck()
-    // Have to figure out why it's not calling my Shuffle function and instead splitting cards in half, in order
+    let deck = new deckOfCards();
+    shuffleDeck(deck)
     let half = Math.ceil(deck.cards.length / 2);
     let playerOneCards = deck.cards.slice(0, half);
     let playerTwoCards = deck.cards.slice(half);
@@ -88,45 +79,59 @@ function playGameOfWar () {
     console.log(playerTwoCards)
 
     cardsRemaining()
+    
+    if(playerOneCards[0].number > playerTwoCards[0].number) {
+        gameAlerts.innerText = "Player 1 wins";
+    } else if(playerTwoCards[0].number > playerOneCards[0].number) {
+        gameAlerts.innerText = "Computer wins";
+    }
+
+
+
     //function to track the remaining cards in each players deck
     function cardsRemaining () {
         playerScore.innerText = playerOneCards.length
         computerScore.innerText = playerTwoCards.length  
+    
     }
     
-    drawCard.addEventListener("click", drawCards) 
-    //function for 'Draw' button to reveal new cards for each player
-    function drawCards() {
-        if(playerOneCards.length !== 0 && playerTwoCards !== 0){
-                let playersCard = playerOneCards.shift();
-                let computersCard = playerTwoCards.shift();
-                if(playersCard.values.indexOf(values[j]) > computersCard.values.indexOf(values[j])) {
-                    //Assign both cards to player's deck
-                    playerOneCards.push(card)
-                } else if(computersCard.values.indexOf(values[j]) > playersCard.values.indexOf(values[j])) {
-                    //Assign both cards to computer's deck
-                    playerTwoCards.push(card)
-                }
-                // console.log(playersCard)
-                // return playersCard computersCard
-
-
-//             // for (let i = 0; i < playerOneCards.length; i++)
-            
-//         }
-//     }
-
-       
-
-
-//     // }
+    //Event Listener for 'Draw' button to reveal new cards for each player
+    drawCard.addEventListener("click", drawCards); 
     
-}
- 
-}
-}
+    function drawCards() {
+        winningConditions()
+        if(playerOneCards.length !== 0 && playerTwoCards !== 0){
+            computersCard.removeChild(computersCard.firstChild);
+            playersCard.removeChild(playersCard.firstChild);
+            computersCard.appendChild(playerTwoCards[0].htmlForCard())
+            playersCard.appendChild(playerOneCards[0].htmlForCard()) 
+        } else if(playerOneCards.length === 0) {
+            //end game function
+        } else if(playerTwoCards.length === 0) {
+            //end game function
 
+        }
+     }
+    function winningConditions() {
+        let removedPlayerOneCard = playerOneCards.shift();
+        let removedPlayerTwoCard = playerTwoCards.shift();
+        if(playerOneCards[0].number > playerTwoCards[0].number) {
+                //Assign both cards to player's deck
+                playerOneCards.push(removedPlayerOneCard, removedPlayerTwoCard);
+                gameAlerts.innerText = "Player 1 wins";
+            } else if(playerTwoCards[0].number > playerOneCards[0].number) {
+                //Assign both cards to computer's deck
+                playerTwoCards.push(removedPlayerTwoCard, removedPlayerOneCard);
+                gameAlerts.innerText = "Computer wins";
+            // } else if(playerTwoCards[0].number === playerOnesCards[0].number) {
+            //     //Play War extended feature function
 
+         } cardsRemaining();
+    }
+    // function endOfGame(){
 
-computersCard.appendChild(deck.cards[0].htmlForCard())
-playersCard.appendChild(deck.cards[0].htmlForCard())
+    // }
+        
+           computersCard.appendChild(playerTwoCards[0].htmlForCard())
+           playersCard.appendChild(playerOneCards[0].htmlForCard())             
+}
